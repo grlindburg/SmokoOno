8fd9cd678f62e6134cd81cc54aa2618715b5ce8e
Grant <grlindburg@gmail.com>
2018-01-17 17:57:47 -0600

    The whole universe.

---
+++ src/app/Controllers/MenuController.js
@@ -0,0 +1,295 @@
+define(
+[
+	'jquery',
+	'underscore',
+	'backbone',
+	'Controllers/TravelController',
+  'Controllers/MoonMenuController',
+	'Modules/TemplateLoader',
+  'Models/Planet',
+  'Models/Moon'
+],
+function(
+  $,
+  _,
+  Backbone,
+  TravelController,
+  MoonMenuController,
+  TemplateLoader,
+  Planet,
+  Moon,
+  MoonData
+) {
+  'use strict';
+
+  const ORBIT_COLOR_DEFAULT = '#ffffff';
+  const ORBIT_COLOR_HIGHLIGHT = '#ffffff';
+  const ORBIT_COLOR_ACTIVE = '#ffffff';
+
+  return Backbone.View.extend({
+    events: {
+      'click a[data-id]': 'onClick',
+      'mouseenter a[data-id]': 'onMouseEnter',
+      'mouseleave a[data-id]': 'onMouseLeave'
+    },
+
+    initialize: function(options) {
+      this.scene = options.scene || null;
+      this.camera = this.scene ? this.scene.camera : null;
+      this.data = options.data || {};
+      this.sceneObjects = options.sceneObjects || [];
+      this.travelController = new TravelController(this.scene);
+      this.templateLoader = new TemplateLoader();
+      this.moonDataModel = null;
+      this.isTraveling = false;
+      this.hasTraveled = false;
+
+      var scene2 = this.scene;
+
+      console.log('hi');
+      var loader = new THREE.OBJLoader();
+
+      var spaceship;
+
+      // load a resource
+      spaceship = loader.load(
+        // resource URL
+        'src/assets/img/Low_poly_UFO.obj',
+        // called when resource is loaded
+        function ( object ) {
+
+          console.log(scene2.camera.position);
+          object.position.set(0, -75, -200);
+          console.log(object.position);
+
+          scene2.camera.add( object );
+
+          scene2.camera.lookAt(object.position);
+          spaceship = object;
+
+          return spaceship;
+
+        },
+        // called when loading is in progresses
+        function ( xhr ) {
+
+          console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+        },
+        // called when loading has errors
+        function ( error ) {
+
+          console.log( 'An error happened' );
+
+        }
+      );
+
+        // instantiate a loader
+        var loader = new THREE.ImageLoader();
+
+        // load a image resource
+        loader.load(
+          // resource URL
+          'src/assets/img/cokpito.png',
+
+          // onLoad callback
+          function ( image ) {
+            // use the image, e.g. draw part of it on a canvas
+            var canvas = document.createElement( 'canvas' );
+            var context = canvas.getContext( '2d' );
+            context.drawImage( image, 100, 100 );
+            console.log("hello");
+
+            console.log(image);
+            canvas.position.set(scene2.camera.position.x,scene2.camera.position.y, 14800);
+            console.log(canvas.position);
+
+            scene2.add( object );
+          },
+
+          // onProgress callback currently not supported
+          undefined,
+
+          // onError callback
+          function () {
+            console.error( 'An error happened.' );
+          }
+        );
+
+      console.log(this.sceneObjects);
+      console.log(this.spaceship);
+      var target = this.matchTarget(3);
+
+      //this.travelController.travelToObject(
+      //  this.scene.camera.parent.position,
+      //  target,
+      //  target.threeDiameter * 2.5
+      //);
+
+      //this.travelController.travelToObject(
+      //  spaceship.position,
+      //  target,
+      //  target.threeDiameter * 2.5
+      //);
+
+      this.currentTarget = options.currentTarget || this.sceneObjects[0];
+      this.template = this.templateLoader.get('planets', 'src/app/Views/menu.twig').then((template)=> {
+        this.template = template;
+        this.render();
+        this.initializePlugins();
+        this.initializeListeners();
+
+        this.moonMenuController = new MoonMenuController({
+          el: $('#moons'),
+          scene: this.scene,
+          data: this.data,
+          sceneObjects: this.sceneObjects.moons
+        });
+      });
+    },
+
+    initializePlugins: function() {
+      //this.accordion = new Foundation.Accordion(this.$('.accordion'), { allowAllClosed: true });
+    },
+
+    render: function() {
+      this.$el.html(this.template.render({ planets: this.data.planets }));
+    },
+
+    onClick: function(e) {
+      var id = Number.parseInt(e.currentTarget.dataset.id);
+      var target = this.matchTarget(id);
+
+      if (this.isCurrentTarget(target)) {
+        e.stopImmediatePropagation();
+        return false;
+      }
+
+      this.travelToObject(target);
+    },
+
+    onMouseEnter: function(e) {
+      var id = Number.parseInt(e.currentTarget.dataset.id);
+      var target = this.matchTarget(id);
+
+      if (this.isCurrentTarget(target)) {
+        if (!this.isTraveling) {
+          this.highlightTarget(target);
+        }
+
+        return true;
+      }
+
+      this.highlightObject(e);
+    },
+
+    onMouseLeave: function(e) {
+      var id = Number.parseInt(e.currentTarget.dataset.id);
+      var target = this.matchTarget(id);
+
+      if (this.isCurrentTarget(target)) {
+        if (!this.isTraveling) {
+          this.unhighlightTarget(target);
+        }
+
+        return true;
+      }
+
+      this.unhighlightObject(e);
+    },
+
+    travelToObject: function(target) {
+      // Return old target to default orbit line color
+      if (this.currentTarget && this.currentTarget.orbitLine) {
+        this.currentTarget.orbitLine.orbit.material.color = new THREE.Color(ORBIT_COLOR_DEFAULT);
+      }
+
+      // Change new target orbit line color
+      target.orbitLine.orbit.material.color = new THREE.Color(ORBIT_COLOR_ACTIVE); // same color as hover and active state
+      target.orbitLine.orbit.material.needsUpdate = true;
+
+      this.travelController.travelToObject(
+        this.scene.camera.parent.position,
+        target,
+        target.threeDiameter * 2.5
+      );
+
+      this.currentTarget = target;
+    },
+
+    matchTarget: function(id) {
+      var target = null;
+
+      for (var i = 0; i < this.sceneObjects.planets.length; i++) {
+        if (this.sceneObjects.planets[i].id === id) {
+          return this.sceneObjects.planets[i];
+      	}
+      }
+
+      return target;
+    },
+
+    isCurrentTarget: function(target) {
+      return this.currentTarget && _.isEqual(this.currentTarget.id, target.id);
+    },
+
+  	highlightObject: function(e) {
+		  var target = this.matchTarget(Number.parseInt(e.currentTarget.dataset.id));
+
+      this.highlightTarget(target);
+      this.highlightOrbit(target);
+    },
+
+    unhighlightObject: function(e) {
+      var target = this.matchTarget(Number.parseInt(e.currentTarget.dataset.id));
+
+      this.unhighlightTarget(target);
+      this.unhighlightOrbit(target);
+    },
+
+    highlightTarget: function(target) {
+      var distanceTo = this.scene.camera.position.distanceTo(target.threeObject.position);
+      var highlightDiameter = distanceTo * 0.011; // 1.1% of distance to target
+
+      target.highlight = highlightDiameter;
+      target.highlight.material.opacity = 0.9;
+    },
+
+    highlightOrbit: function(target) {
+      var hightlightColor = '#ffffff'; // target.orbitHighlightColor || #216883
+
+      target.orbitLine.orbit.material.color = new THREE.Color(ORBIT_COLOR_HIGHLIGHT); // new THREE.Color('#d3d3d3');
+      target.orbitLine.orbit.material.needsUpdate = true;
+    },
+
+    unhighlightTarget: function(target) {
+      target.core.remove(target.highlight);
+    },
+
+    unhighlightOrbit: function(target) {
+      target.orbitLine.orbit.material.color = new THREE.Color(ORBIT_COLOR_DEFAULT);
+      target.orbitLine.orbit.material.needsUpdate = true;
+    },
+
+    initializeListeners: function() {
+      document.addEventListener('solarsystem.travel.planet.start', this.handleTravelStart.bind(this));
+      document.addEventListener('solarsystem.travel.planet.complete', this.handleTravelComplete.bind(this));
+      document.addEventListener('solarsystem.focalpoint.change', this.handleTravelComplete.bind(this));
+    },
+
+    handleTravelStart: function(e) {
+      this.isTraveling = true;
+
+      $('#current-target-title').removeClass('active').html('');
+    },
+
+    handleTravelComplete: function(e) {
+      var object = e.detail;
+
+      $('#current-target-title').html(object.name).addClass('active');
+
+      this.moonMenuController.setModel(object._moons);
+      this.isTraveling = false;
+    }
+  });
+});
---
+++ src/app/Controllers/TravelController.js
@@ -0,0 +1,254 @@
+define(
+[
+  'Models/Moon',
+  'Modules/ColorManager'
+],
+function(Moon, ColorManager) {
+  'use strict';
+
+  class TravelController {
+    constructor(scene) {
+      this.scene = scene;
+      this.camera = this.scene.camera;
+      this.travelObjectType = 'planet'; // default
+      this.travelStartEvent = new CustomEvent('solarsystem.travel.start');
+      this.targetPosition = new THREE.Vector3();
+      this.colorManager = new ColorManager();
+    }
+
+    /**
+     * @param  {Number} radius              [The target object's orbit amplitude (distance from parent)]
+     * @param  {Number} theta               [The day of the year in the form of radians]
+     * @param  {Number} distanceFromParent  []
+     * @return {Object}
+     */
+    calculateDestinationCoordinates_(radius, theta, distanceFromParent) {
+      // var d = distanceFromParent + (distanceFromParent / 2);
+      var r = radius;
+      var x = r * Math.cos(theta);
+      var y = r * Math.sin(theta);
+
+      return {
+        x: x,
+        y: y,
+        z: 0
+      };
+    }
+
+    calculateDestinationCoordinates(targetObject) {
+      var x = targetObject.core.position.x;
+      var y = targetObject.core.position.y;
+      var z = targetObject.core.position.z;
+
+      var destinationX = x;
+      var destinationY = y;
+      var destinationZ = z;
+
+      var quadrant1 = x > 0 && y > 0;
+      var quadrant2 = x < 0 && y > 0;
+      var quadrant3 = x < 0 && y < 0;
+      var quadrant4 = x > 0 && y < 0;
+
+      var offset = targetObject.threeDiameter > 3 ? targetObject.threeDiameter * 6 : targetObject.threeDiameter * 3;
+
+      if (quadrant1) {
+        destinationX = destinationX + offset;
+        destinationY = destinationY + offset;
+      }
+
+      if (quadrant2) {
+        destinationX = destinationX - offset;
+        destinationY = destinationY + offset;
+      }
+
+      if (quadrant3) {
+        destinationX = destinationX - offset;
+        destinationY = destinationY - offset;
+      }
+
+      if (quadrant4) {
+        destinationX = destinationX + offset;
+        destinationY = destinationY - offset;
+      }
+
+      return {
+        x: destinationX,
+        y: destinationY,
+        z: destinationZ + (targetObject.threeDiameter * 0.15)
+      };
+    }
+
+    dispatchTravelStartEvent(data) {
+      var event = new CustomEvent('solarsystem.travel.'+ this.travelObjectType +'.start', {
+        detail: data
+      });
+
+      document.dispatchEvent(event);
+    }
+
+    dispatchTravelCompleteEvent(data) {
+      var event = new CustomEvent('solarsystem.travel.'+ this.travelObjectType +'.complete', {
+        detail: data
+      });
+
+      document.dispatchEvent(event);
+    }
+
+    prepareForTravel(takeOffHeight, targetObject) {
+      var travelDuration = 3000; // 3000;
+
+      return new TWEEN.Tween(this.camera.position)
+        .to({
+          x: this.camera.position.x,
+          y: this.camera.position.y,
+          z: this.camera.position.z + 700
+        }, travelDuration)
+        .easing(TWEEN.Easing.Cubic.InOut)
+        .onUpdate((currentAnimationPosition)=> {
+            this.camera.lookAt(targetObject.threeObject.position);
+        })
+      ;
+    }
+
+    travelToObject(currentPosition, targetObject, takeOffHeight) {
+      var analyser;
+
+      var travelDuration = 5000; // 5000; // milliseconds
+
+      this.travelObjectType = targetObject instanceof Moon ? 'moon' : 'planet';
+      this.dispatchTravelStartEvent(targetObject);
+
+      THREE.SceneUtils.detach(this.camera, this.camera.parent, this.scene);
+      THREE.SceneUtils.attach(this.camera, this.scene, targetObject.orbitCentroid);
+
+      //targetObject.core.updateMatrixWorld();
+      //targetObject.orbitCentroid.updateMatrixWorld();
+
+      this.camera.lookAt(targetObject.threeObject.position);
+
+      var destinationCoordinates = this.calculateDestinationCoordinates(targetObject);
+      //var takeOff = this.prepareForTravel(takeOffHeight, targetObject);
+
+      var cameraTarget = targetObject instanceof Moon ? targetObject.core : targetObject.objectCentroid;
+
+      var listener = new THREE.AudioListener();
+      this.camera.add( listener );
+
+      window.addEventListener('touchstart', this.playSound(targetObject, listener));
+
+      console.log(targetObject);
+
+      // get the average frequency of the sound
+      //var data = analyser.getFrequencyData();
+
+      //return takeOff.start().onComplete(()=> {
+        var cameraTween = new TWEEN.Tween(this.camera.position)
+          .to(destinationCoordinates, travelDuration)
+          .easing(TWEEN.Easing.Cubic.InOut)
+          .onUpdate(function(currentAnimationPosition) {
+            var destinationCoordinates = this.calculateDestinationCoordinates(targetObject);
+
+            cameraTween.to(destinationCoordinates);
+
+            this.camera.lookAt(targetObject.threeObject.position);
+
+            //if (targetObject.highlight.geometry.boundingSphere.radius > targetObject.threeDiameter / 1.25) {
+            //  this.updateTargetHighlight(targetObject);
+            //}
+          }.bind(this))
+          .onComplete(this.handleComplete.bind(this, targetObject, cameraTarget, analyser))
+          .start()
+        ;
+      //});
+    }
+
+    handleComplete(targetObject, cameraTarget, analyser) {
+      cameraTarget = cameraTarget || targetObject.objectCentroid;
+
+      THREE.SceneUtils.detach(this.camera, this.camera.parent, this.scene);
+      THREE.SceneUtils.attach(this.camera, this.scene, cameraTarget);
+
+      var transition = this.colorManager.fadeTo(
+        targetObject.highlight,
+        targetObject.highlight.material.color,
+        { r: 59, b: 234, b: 247 },
+        3000
+      ).onComplete(()=> {
+        targetObject.core.remove(targetObject.highlight);
+      });
+
+      this.camera.lookAt(new THREE.Vector3());
+
+      //console.log(analyser.data);
+      //var uniforms = {
+      //  tAudioData: { value: new THREE.DataTexture( analyser.data, 128 / 2, 1, THREE.LuminanceFormat ) }
+      //};
+      //console.log(uniforms);
+
+      //targetObject.threeObject.scale.set(Math.random() * 1000);
+
+      targetObject.core.updateMatrixWorld();
+      targetObject.orbitCentroid.updateMatrixWorld();
+
+      this.dispatchTravelCompleteEvent(targetObject);
+
+    }
+
+    playSound(targetObject, listener) {
+      
+
+      // create a global audio source
+      var sound = new THREE.PositionalAudio( listener );
+
+      // load a sound and set it as the Audio object's buffer
+      var audioLoader = new THREE.AudioLoader();
+      var track;
+
+      switch(targetObject.name) {
+        case "Earth":
+          track = 'src/assets/sound/nolook.mp3';
+
+          break;
+
+        case "Mars":
+          track = 'src/assets/sound/superstarmoves.mp3';
+
+          break;
+      }
+
+      audioLoader.load( track, function( buffer ) {
+        sound.setBuffer( buffer );
+        sound.setRefDistance(3)
+        sound.setLoop( true );
+        sound.setVolume( 0.5 );
+        sound.play();
+      });
+
+      var source = listener.context.createBufferSource();
+      source.connect(listener.context.destination);
+      source.start();
+
+      //alert(source);
+      targetObject.threeObject.add(sound);
+
+    }
+
+    /**
+     * Updates the target's highlight geometry based on the camera's
+     * distance from the target.
+     *
+     * @param {Object} target
+     */
+    updateTargetHighlight(target) {
+      target.core.remove(target.highlight);
+
+      var distanceTo = this.camera.position.distanceTo(target.threeObject.position);
+      var highlightDiameter = distanceTo * 0.011; // 1.1% of distance to target
+
+      target.highlight = highlightDiameter;
+      target.highlight.material.opacity = 0.9;
+    }
+  }
+
+  return TravelController;
+});
